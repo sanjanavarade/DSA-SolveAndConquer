80b287d8cc38d8ef58fac49732dad1de3d789f38
sanjanavarade <127649474+sanjanavarade@users.noreply.github.com>
2025-12-30 22:00:47 +0530

    Time: 73 ms (34.28%) | Memory: 44.7 MB (73.31%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,18 +1,24 @@
+import java.util.*;
+
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum=0;
-        for(int num:nums)sum+=num;
-        if(sum%2!=0)return false;
-        sum=sum/2;
-        boolean[] dp= new boolean[sum+1];
-        dp[0]= true;
-        for(int num:nums){
-            for(int j=sum;j>=num;--j){
-                dp[j]=dp[j]||dp[j-num];
-                if(dp[sum]) return true;
-            }
-        }
-        return dp[sum];
+        int sum = 0, n = nums.length;
         
+        for (int i = 0; i < n; i++)
+            sum += nums[i];
+        
+        if (sum % 2 != 0) 
+            return false;
+
+        boolean[] dp = new boolean[10001];
+        dp[0] = true;
+        for (int num : nums) {
+            for (int j = 10000; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+            if (dp[sum / 2]) 
+                return true;
+        }
+        return dp[sum / 2];
     }
 }
