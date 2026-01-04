5180a080fb6114075a0e3ddc8063e926dfa99151
sanjanavarade <127649474+sanjanavarade@users.noreply.github.com>
2026-01-04 16:14:48 +0530

    Time: 18 ms (95.43%) | Memory: 51.7 MB (86.52%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -1,56 +1,51 @@
 class Solution {
-    public String shortestCommonSupersequence(String str1, String str2) {
-        // Step 1: Find the longest common subsequence using dynamic programming
-        int m = str1.length();
-        int n = str2.length();
-        int[][] dp = new int[m + 1][n + 1];
-        
-        // Fill the dp table
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+    public String shortestCommonSupersequence(String s1, String s2) {
+        if (s1.equals(s2)) return s1;
+
+        int m = s1.length(), n = s2.length();
+        int[][] dp = new int[m][n];
+
+        // LCS DP
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    dp[i][j] = 1 + ((i > 0 && j > 0) ? dp[i - 1][j - 1] : 0);
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.max(
+                        (i > 0 ? dp[i - 1][j] : 0),
+                        (j > 0 ? dp[i][j - 1] : 0)
+                    );
                 }
             }
         }
-        
-        // Step 2: Construct the shortest common supersequence
-        // Start from the bottom right of the dp table
-        int i = m, j = n;
-        StringBuilder result = new StringBuilder();
-        
-        while (i > 0 && j > 0) {
-            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                // If the characters are the same, add it once
-                result.append(str1.charAt(i - 1));
-                i--;
+
+        // Build SCS
+        StringBuilder sb = new StringBuilder();
+        int i = m - 1, j = n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (s1.charAt(i) == s2.charAt(j)) {
+                sb.append(s1.charAt(i));
+                i--; j--;
+            } else if (j > 0 && (i == 0 || dp[i][j - 1] > dp[i - 1][j])) {
+                sb.append(s2.charAt(j));
                 j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1]) {
-                // If coming from top has higher value, take character from str1
-                result.append(str1.charAt(i - 1));
-                i--;
             } else {
-                // Otherwise, take character from str2
-                result.append(str2.charAt(j - 1));
-                j--;
+                sb.append(s1.charAt(i));
+                i--;
             }
         }
-        
-        // Add remaining characters from str1 (if any)
-        while (i > 0) {
-            result.append(str1.charAt(i - 1));
+
+        while (i >= 0) {
+            sb.append(s1.charAt(i));
             i--;
         }
-        
-        // Add remaining characters from str2 (if any)
-        while (j > 0) {
-            result.append(str2.charAt(j - 1));
+
+        while (j >= 0) {
+            sb.append(s2.charAt(j));
             j--;
         }
-        
-        // Reverse the result to get the final supersequence
-        return result.reverse().toString();
+
+        return sb.reverse().toString();
     }
 }
